2bc6fdf9137dae151dcb85911756f35093a1b7fa
Sida Zhang <a_zhangsida@126.com>
2017-04-10 21:43:43 +0800

    Create pipelines.py

---
+++ pipelines.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+class PeoplesdailyPipeline(object):
+    def open_spider(self, spider):
+        self.f = open('anarticle.txt', 'a')
+
+    def close_spider(self, spider):
+        self.f.close()
+
+    def process_item(self, item, spider):
+        try:
+            line = str(dict(item))+"\n"
+            self.f.write(line)
+        except:
+            pass
