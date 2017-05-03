0bc70f92be80ec61b4ba4c256f5a6795d2638b86
Sida Zhang <a_zhangsida@126.com>
2017-05-03 18:05:43 +0800

    Update items.py

--- items.py
+++ items.py
@@ -5,6 +5,8 @@ import scrapy
 class PeoplesdailyItem(scrapy.Item):
     date = scrapy.Field()
     page = scrapy.Field()
+    news_type = scrapy.Field()
     title = scrapy.Field()
+    subtitle = scrapy.Field()
     author = scrapy.Field()
     text = scrapy.Field()
