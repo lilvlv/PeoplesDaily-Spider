5eeb3225347d970c65f208b8b19975e5e2b65c2e
Sida Zhang <a_zhangsida@126.com>
2017-05-03 17:59:40 +0800

    Update pd.py
    1.移除Beautiful Soup库，改用css selector
    2.修改多个parse*方法

--- spiders/pd.py
+++ spiders/pd.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
-from bs4 import BeautifulSoup
-import bs4
-from PeoplesDaily.items import PeoplesdailyItem
-import re
 import datetime
+from PeoplesDaily.items import PeoplesdailyItem
 
 
 class PdSpider(scrapy.Spider):
@@ -24,34 +21,27 @@ class PdSpider(scrapy.Spider):
             yield self.make_requests_from_url(url)
 
     def parse(self, response):
-        soup = BeautifulSoup(response.text, 'html.parser')
-        pages = int(soup.find(id="UseRmrbPageNum").string)
-        url_split_list = response.url.split("/")[:-1]
-        for num in range(pages):
-            temp = url_split_list + [str(num+1)]
-            url = "/".join(temp)
-            yield scrapy.Request(url, callback = self.parse_a_page_for_url)
-
+        url_invariant_part = r'http://58.68.146.102'
+        url_variable_parts = response.css(".banci_hover h3 a::attr(href)").extract()
+        for a_variable_part in url_variable_parts:
+            url = url_invariant_part + a_variable_part
+            yield scrapy.Request(url, callback=self.parse_a_page_for_url)
+            
     def parse_a_page_for_url(self, response):
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for li in soup.find('ul').children:
-            if isinstance(li, bs4.element.Tag):
-                url = self.url_invariant_part+li.a.get('href')
-                yield scrapy.Request(url, callback = self.parse_an_article)
-
+        url_invariant_part = r'http://58.68.146.102'
+        url_variable_parts = response.css(".title_list h3 a::attr(href)").extract()
+        for a_variable_part in url_variable_parts:
+            url = url_invariant_part + a_variable_part
+            yield scrapy.Request(url, callback=self.parse_an_article)
+            
     def parse_an_article(self, response):
-        soup = BeautifulSoup(response.text, 'html.parser')
 
         article = PeoplesdailyItem()
-        article['date'] = response.url.split('/')[4]
-        article['page'] = response.url.split('/')[5]
-        article['title'] = []
-        for title in soup.find_all(attrs = {'class': re.compile('title')}):
-            article['title'].append(title.text)
-        article['author'] = []
-        for author in soup.find_all(attrs = {'class': re.compile('author')}):
-            article['author'].append(author.text)
-        article['text'] = []
-        for p in soup.find_all('p'):
-            article['text'].append(p.text)
+        article['date'] = response.css(".sha_left span::text").extract()[0]
+        article['page'] = response.css(".sha_left span::text").extract()[1]
+        article['type'] = response.css(".sha_left span::text").extract()[2]
+        article['title'] = response.css(".title").extract()
+        article['subtitle'] = response.css(".subtitle").extract()
+        article['author'] = response.css(".author").extract()
+        article['text'] = response.css("#FontZoom>p::text").extract()
         return article
