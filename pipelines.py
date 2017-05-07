81397cfb365845b3632b7e81b38fdeba1601bc2d
Sida Zhang <a_zhangsida@126.com>
2017-05-07 17:39:26 +0800

    Update pipelines.py
    使用ImagesPipeline

--- pipelines.py
+++ pipelines.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+import scrapy.pipelines.images
+
+class RenameImagesPipeline(scrapy.pipelines.images.ImagesPipeline):
+    def file_path(self, request, response=None, info=None):
+        temp = request.url.split('/')
+        image_guid = temp[-2]+"_"+temp[-1]
+        return '%s.jpg' % (image_guid)
 
 class PeoplesdailyPipeline(object):
     def open_spider(self, spider):
